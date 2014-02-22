7614bfb3967ff12419b3e8be895e4e56e5784cde
Prita Kumar <prita.kumar@gmail.com>
2014-02-22 21:06:27 +0000

    hook up database

---
+++ db/migrate/20140222195431_create_pics.rb
@@ -0,0 +1,13 @@
+class CreatePics < ActiveRecord::Migration
+  def change
+    create_table :pics do |t|
+
+
+      t.string :title
+      t.string :subtitle
+      t.text :message
+
+      t.timestamps
+    end
+  end
+end
